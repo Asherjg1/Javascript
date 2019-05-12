53224315b9424bfdf7ebe8a0e653feb7bd1d63a0
Asherjg1 <asherjg1@gmail.com>
2019-05-12 13:19:56 -0700

    yeet

---
+++ lesson.js
@@ -0,0 +1,12 @@
+var x=0
+
+for (x=0; x<220; x++){
+  if  (((x %2) == 0)){
+    console.log(x);
+    console.log("this is even");;
+  }
+  else{
+    console.log("this is odd");;
+
+  console.log(x);}
+}
